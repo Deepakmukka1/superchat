45c23d9a14f3e76780293fb09ac01a9c317fe185
Mukka Deepak <ramu92013@gmail.com>
2021-12-31 23:26:44 +0530

    ⚡️ Add firestore write feature

--- src/components/Chatroom/index.js
+++ src/components/Chatroom/index.js
@@ -1,24 +1,48 @@
+/* eslint-disable no-unused-expressions */
 /* eslint-disable jsx-a11y/alt-text */
-import React from 'react'
-import Navbar from '../Navbar'
-import { getFirestore, collection} from 'firebase/firestore';
-import { useCollection } from 'react-firebase-hooks/firestore';
+import React from "react";
+import Navbar from "../Navbar";
+import {
+  getFirestore,
+  collection,
+  orderBy,
+  limit,
+  addDoc,
+  serverTimestamp,
+} from "firebase/firestore";
+import { useCollection } from "react-firebase-hooks/firestore";
 
-const Chatroom = ({auth,app}) => {
-    // console.log(auth)
-    const {currentUser}=auth
-    const [messages, loading, error] = useCollection(
-        collection(getFirestore(app), 'messages'),
-        {
-          snapshotListenOptions: { includeMetadataChanges: true },
-        }
-      );
+const Chatroom = ({ auth, app }) => {
+  // console.log(auth)
+  const { currentUser } = auth;
+  const [messages, loading, error] = useCollection(
+    collection(getFirestore(app), "messages"),
+    orderBy("createdAt", limit(25))
+  );
 
-    return (
-        <div>
-            <Navbar name={currentUser.displayName} imageURL={currentUser.photoURL} auth={auth}/>
-            <h3>Chatroom</h3>
-            <p>
+  const wirteData = async () => {
+    const db = getFirestore(app);
+
+    const docRef = await addDoc(collection(db, "messages"), {
+      message: "new-message",
+      createdAt: serverTimestamp(),
+      uid: currentUser.uid,
+      photoURL: currentUser.photoURL,
+    });
+
+    console.log(docRef.id);
+    // setDoc(docRef, { capital: true }, { merge: true });
+  };
+
+  return (
+    <div>
+      <Navbar
+        name={currentUser.displayName}
+        imageURL={currentUser.photoURL}
+        auth={auth}
+      />
+      <h3>Chatroom</h3>
+      <p>
         {error && <strong>Error: {JSON.stringify(error)}</strong>}
         {loading && <span>Collection: Loading...</span>}
         {messages && (
@@ -31,9 +55,9 @@ const Chatroom = ({auth,app}) => {
           </span>
         )}
       </p>
+      <button onClick={wirteData}>Write</button>
+    </div>
+  );
+};
 
-        </div>
-    )
-}
-
-export default Chatroom
+export default Chatroom;
