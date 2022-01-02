f351f9c7176aa036e4681d7456431937019a28dc
Mukka Deepak <ramu92013@gmail.com>
2022-01-02 14:26:57 +0530

    🔨 v1.0

--- src/components/Signin/index.js
+++ src/components/Signin/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import GoogleIcon from '../../assets/Googleicon.jsx'
 import Icon from "../../assets/Chat.jsx"
+import './Signin.css'
 const Signin = ({ auth }) => {
   const signInWithGoogle = () => {
     const provider = new GoogleAuthProvider();
@@ -8,9 +10,14 @@ const Signin = ({ auth }) => {
   };
 
   return (
-    <div>
-      <Icon/>
-      <button onClick={signInWithGoogle}>Signin with google</button>
+    <div className="signin">
+      <div className="icon">
+      <Icon />
+      </div>
+      <div>
+      <h2>Welcome to group chat 🎉✨</h2>
+      <button onClick={signInWithGoogle} className="btn" style={{display:'flex',width:'12rem',alignItems:'center',justifyContent:'center'}}><GoogleIcon/> Signin with google</button>
+      </div>
     </div>
   );
 };
