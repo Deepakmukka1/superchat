580c57f73ffa9f3376821ad05058f4b9d02f96a2
Mukka Deepak <ramu92013@gmail.com>
2022-01-02 14:03:32 +0530

    🚑 Remove SVG bug

--- src/components/Signin/index.js
+++ src/components/Signin/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import Icon from "../../assets/Chat.jsx"
 const Signin = ({ auth }) => {
   const signInWithGoogle = () => {
     const provider = new GoogleAuthProvider();
@@ -8,6 +9,7 @@ const Signin = ({ auth }) => {
 
   return (
     <div>
+      <Icon/>
       <button onClick={signInWithGoogle}>Signin with google</button>
     </div>
   );
