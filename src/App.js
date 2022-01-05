643e7b73e1e822845d1ff39d248f71731e307031
Mukka Deepak <ramu92013@gmail.com>
2022-01-05 12:23:43 +0530

    🔒️ Add env variables

--- src/App.js
+++ src/App.js
@@ -6,13 +6,13 @@ import { initializeApp } from "firebase/app";
 // import firebase from 'firebase/app'
 import Chatroom from "./components/Chatroom";
 import Signin from "./components/Signin";
-import { firebaseConfig } from "./config/firebase";
+// import { firebaseConfig } from "./config/firebase";
 
 // Import the functions you need from the SDKs you need
 
 // Initialize Firebase
 
-const app = process.env.NODE_ENV==="development"?initializeApp(firebaseConfig):initializeApp(JSON.parse(process.env.FIREBASE_CONFIG))
+const app = initializeApp(JSON.parse(process.env.FIREBASE_CONFIG))
 
 const auth = getAuth(app);
 
